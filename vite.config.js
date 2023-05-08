01f1fcebf3fe035924c72637cdb569b857b3d412
Eskander JGUIRIM <47850204+eskander1998@users.noreply.github.com>
2023-05-09 01:13:39 +0200

    configure build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from "@vitejs/plugin-vue2";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ToDo/',
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "ToDo",
+      fileName: (format) => `todo.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
  
 });
