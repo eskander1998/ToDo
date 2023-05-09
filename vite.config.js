da0260e81053c77956c3e39c520e25d146786e24
Eskander JGUIRIM <47850204+eskander1998@users.noreply.github.com>
2023-05-09 15:05:35 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/ToDo',
+  base: '/',
   // build: {
   //   lib: {
   //     entry: resolve(__dirname, "src/index.js"),
