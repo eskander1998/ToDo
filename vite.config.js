2f1e2e9245e97dab776994dfb0f7452ee089e07a
Eskander JGUIRIM <47850204+eskander1998@users.noreply.github.com>
2023-05-09 15:01:32 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,19 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.js"),
-      name: "todo",
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-  },
+  base: '/ToDo',
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, "src/index.js"),
+  //     name: "todo",
+  //   },
+  //   rollupOptions: {
+  //     external: ["vue"],
+  //     output: {
+  //       globals: {
+  //         vue: "Vue",
+  //       },
+  //     },
+  //   },
+  // },
 })
